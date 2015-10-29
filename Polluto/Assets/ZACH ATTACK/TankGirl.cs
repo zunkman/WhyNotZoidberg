da8c180b9ff841ca3526b544e00b3d45650aba70
zacharymacleod <zachary.macleod@gmail.com>
2015-10-29 16:03:02 -0400

    weh

--- Polluto/Assets/ZACH ATTACK/TankGirl.cs
+++ Polluto/Assets/ZACH ATTACK/TankGirl.cs
@@ -70,60 +70,7 @@ public class TankGirl : Player
     }
 
 
-    protected override bool FloorCollide()
-    {
-        bool canjump = false;
-        bool floor = false;
-        float oldyspeed = speed.y;
-        RaycastHit hit;
-
-
-        for (int i = 0; i <= 2; i++)
-        {
-            float xoff = (width / 2) * (i - 1);
-
-            if (Physics.Raycast(transform.position + new Vector3(xoff, magicNumber, 0f), Vector3.down, out hit, Mathf.Abs(speed.y * Time.deltaTime) + magicNumber) && speed.y <= 0)
-            {
-                //snap to floor, and enable jumping.
-                transform.position += new Vector3(0f, -hit.distance + magicNumber, 0f);
-                speed.y = 0f;
-                if (isJumping) {
-                    stun = fallDelay;
-                    //camera.setOffset(new Vector3(0f, -1f, 0f));
-                    speed.x = 0f;
-                }
-                canjump = true;
-                isJumping = false;
-                floor = true;
-            }
-        }
-
-        if (floor)
-        {
-            RaycastHit h1, h2;
-            if (Physics.Raycast(transform.position + new Vector3(-width / 2, height - magicNumber, 0f), Vector3.down, out h1) &&
-                Physics.Raycast(transform.position + new Vector3(width / 2, height - magicNumber, 0f), Vector3.down, out h2))
-            {
-
-                float slope = Vector2.Angle(h2.point - h1.point, Vector2.right);
-                Debug.Log(h1.point.ToString() + " " + h2.point.ToString());
-                Debug.Log(slope);
-                if (slope > maxSlope)
-                {
-                    canjump = false;
-                    if (h1.distance < h2.distance) { transform.Translate(-oldyspeed * Mathf.Atan(slope * Mathf.Deg2Rad) * Time.deltaTime + magicNumber, 0f, 0f); speed.x = Mathf.Max(0f, speed.x); speed.y = oldyspeed; }
-                    else { transform.Translate(oldyspeed * Mathf.Atan(slope * Mathf.Deg2Rad) * Time.deltaTime - magicNumber, 0f, 0f); speed.x = Mathf.Min(0f, speed.x); speed.y = oldyspeed; }
-
-                }
-
-
-            }
-
-
-        }
-
-        return canjump;
-    }
+    
 
     void Attack() { 
     
