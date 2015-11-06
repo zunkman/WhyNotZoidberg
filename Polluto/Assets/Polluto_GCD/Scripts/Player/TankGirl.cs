d58659869f4054e8dd7f2e4f2e2ff1cfe202007a
zacharymacleod <zachary.macleod@gmail.com>
2015-11-06 13:26:59 -0500

    Tank Girl Stuff For Realsies This Time I Hope
    didn't commit properly the first time, apparently.

--- Polluto/Assets/Polluto_GCD/Scripts/Player/TankGirl.cs
+++ Polluto/Assets/Polluto_GCD/Scripts/Player/TankGirl.cs
@@ -16,9 +16,26 @@ public class TankGirl : Player
     PlayerFollow camera;
     public float attackDamage;
 
+    float chargeMult = 1.5f;
+
+    float attackWindUp = 0.1f;
+    float attackWindDown = 0.65f;
+    float chargeWindDown = 0.5f;
+    float chargeAttackWindDown = 1.2f;
+    float attackDuration = 0.25f;
+
+    float attackPrepare;
+
+    bool isAttacking = false;
+    public bool charging = false;
+    int chargeDir = 0;
+
+    TankgirlAttack attack;
+
 
     protected override void Start()
     {
+        attack = GetComponentInChildren<TankgirlAttack>();
         speed = Vector3.zero;
         jumptimer = jumpDelay;
         camera = GameObject.Find("Main Camera").GetComponent<PlayerFollow>();
@@ -26,10 +43,28 @@ public class TankGirl : Player
 
     protected override void Update()
     {
-        if (playerNumber == 1) inputDirection.x = Input.GetAxis("Horizontal");
-        if (playerNumber == 2) inputDirection.x = Input.GetAxis("Horizontal 2");
-        if (playerNumber == 1) inputDirection.y = Input.GetAxis("Vertical");
-        if (playerNumber == 2) inputDirection.y = Input.GetAxis("Vertical 2");
+        if (playerNumber == 1)
+        {
+            inputDirection.x = Input.GetAxis("Horizontal");
+            inputDirection.y = Input.GetAxis("Vertical");
+            if(Input.GetButton("Basic Attack") && stun<0f){
+                Attack();
+            }
+            if (Input.GetButton("Special Attack") && stun < 0f) {
+                Charge();
+            }
+
+        }
+        if (playerNumber == 2)
+        {
+            inputDirection.x = Input.GetAxis("Horizontal 2");
+            inputDirection.y = Input.GetAxis("Vertical 2");
+            if (Input.GetButton("Basic Attack 2") && stun < 0f)
+            {
+                Attack();
+            }
+
+        } 
         if (inputDirection != Vector2.zero) inputDirection.Normalize();
 
         if (stun >= 0f) { stun -= Time.deltaTime; }
@@ -41,8 +76,12 @@ public class TankGirl : Player
                 jumptimer = jumpDelay;
                 speed.y = jumpspeed;
                 speed.x = (horspeed + JumpBonusSpeed) * Input.GetAxis("Horizontal");
+                if (charging) {
+                    speed.x *= chargeMult;
+                }
                 aboutToJump = false;
                 isJumping = true;
+                charging = false;
             }
         }
 
@@ -51,12 +90,25 @@ public class TankGirl : Player
             if (!isJumping && stun < 0f)
             {
                 HorMove();
+                if (charging)
+                {
+                    speed.x *= chargeMult;
+                    Charging();
+                }
+                
+            }
+            if (stun > 0f) {
+                if (speed.x > 0) { speed.x = Mathf.Max(speed.x - horaccel/4f * Time.deltaTime, 0); }
+                else { speed.x = Mathf.Min(speed.x + horaccel/4f * Time.deltaTime, 0); }
             }
         }
 
         Gravity();
 
         bool canjump = FloorCollide();
+        if (canjump == true && isJumping) { 
+        
+        }
 
         if (playerNumber == 1)
         {
@@ -81,19 +133,71 @@ public class TankGirl : Player
 
         CeilCollide();
         transform.localPosition += speed * Time.deltaTime;
+
+        if (isAttacking) {
+            attackPrepare -= Time.deltaTime;
+            if (attackPrepare < 0) {
+                if (attack.isActive)
+                {
+                    attack.isActive = false;
+                    isAttacking = false;
+                }
+                else {
+                    attack.isActive = true;
+                    attackPrepare = attackDuration;
+                }
+            }
+        }
+
     }
 
     protected override void Jump()
     {
         aboutToJump = true;
         speed.x = 0;
+        
     }
 
 
     
 
-    void Attack() { 
-    
+    void Attack() {
+        if (!charging)
+        {
+            stun = attackWindUp + attackWindDown;
+            isAttacking = true;
+            attackPrepare = attackWindUp;
+        }
+        else {
+            stun = chargeAttackWindDown;
+            attack.isActive = true;
+            attackPrepare = attackDuration;
+            isAttacking = true;
+            charging = false;
+        }
+        //speed.x = 0;
     }
+    void Charge() {
+        if (speed.x != 0) {
+            charging = true;
+            chargeDir = (int)Mathf.Sign(speed.x);
+        }
+    }
+    void Charging() {
+        if (playerNumber == 1) {
+            if (Mathf.Sign(Input.GetAxis("Horizontal")) != chargeDir) {
+                charging = false;
+                stun = chargeWindDown;
+            }
+        }
 
+        if (playerNumber == 2)
+        {
+            if (Mathf.Sign(Input.GetAxis("Horizontal 2")) != chargeDir)
+            {
+                charging = false;
+                stun = chargeWindDown;
+            }
+        }
+    }
 }
--- Polluto/Assets/TankgirlAttack.cs
+++ Polluto/Assets/TankgirlAttack.cs
@@ -4,23 +4,39 @@ using System.Collections;
 public class TankgirlAttack : MonoBehaviour {
 
     private float damage;
+    public bool isActive;
+    ArrayList hitObjects;
+    int chargebonus;
     // Use this for initialization
     void Start()
     {
+        hitObjects = new ArrayList();
         damage = this.GetComponentInParent<TankGirl>().attackDamage;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        GetComponent<MeshRenderer>().enabled = isActive;
+        if (!isActive) {
+            hitObjects.Clear();
+        }
+        if (this.GetComponentInParent<TankGirl>().charging) { chargebonus = 2; } else { chargebonus = 1; }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Enemy")
+        
+        if (other.gameObject.tag == "Enemy" && isActive)
         {
-            other.gameObject.GetComponent<EnemyDamage>().health -= damage;
+
+            if (!hitObjects.Contains(other.gameObject)) { 
+
+                other.gameObject.GetComponent<EnemyDamage>().health -= damage*chargebonus;
+                hitObjects.Add(other.gameObject);
+            }
+            
+
         }
     }
 
