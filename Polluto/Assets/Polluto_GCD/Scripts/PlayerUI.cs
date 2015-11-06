613f8566f91cac5aed779f8a0ac8f21535bcd4cd
joeleon <joe9leon@hotmail.com>
2015-11-06 13:02:10 -0500

    Initial UI changes and health/damage updates
    Altered bomb, damage, Higgs damage and health, turret damage, health,
    and projectile speed. Created UI prefab.

---
+++ Polluto/Assets/Polluto_GCD/Scripts/PlayerUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField] private GameObject playerOne, playerTwo, gameHandler;
+    private bool twoPlayers;
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
