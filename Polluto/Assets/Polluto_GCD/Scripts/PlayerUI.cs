430b013c5fc83cf1a13db1de77eed2e0b743e14f
joeleon <joe9leon@hotmail.com>
2015-11-17 13:14:15 -0500

    Player UI Fully Functional
    UI Updates Health, Player Name and Lives Now. Bug discovered with Tank
    Girl, who doesn't die, and GameHandler, where if there are two players,
    both will be respawned till lives is 0, at which point menu is loaded.

--- Polluto/Assets/Polluto_GCD/Scripts/PlayerUI.cs
+++ Polluto/Assets/Polluto_GCD/Scripts/PlayerUI.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private GameObject playerOne, playerTwo, gameHandler;
-    private bool twoPlayers;
-    [SerializeField] private Text playerOneName, playerTwoName, playerOneHealth, playerTwoHealth;
-    [SerializeField] private Image playerOneBasic, playerTwoBasic, playerOneSpecial, playerTwoSpecial;
+    [SerializeField] private Text playerOneName, playerTwoName, lives, missionObjective;
+    [SerializeField] private Image playerOneBasic, playerTwoBasic, playerOneSpecial, playerTwoSpecial, playerOneHealth, playerTwoHealth, playerOneEmpty, playerTwoEmpty;
+    [SerializeField] private float objectiveShowTime;
 
+    private bool twoPlayers, pauseOn;
     
+
     // Use this for initialization
     void Start ()
     {
         gameHandler = GameObject.FindGameObjectWithTag("gameHandler");
+        pauseOn = false;
 
         if (gameHandler.GetComponent<GameHandler>().getNumPlayers() == 2)
         {
@@ -32,6 +35,22 @@ public class PlayerUI : MonoBehaviour
         playerTwoBasic.type = Image.Type.Filled;
         playerTwoBasic.fillMethod = Image.FillMethod.Vertical;
 
+        playerOneHealth.type = Image.Type.Filled;
+        playerOneHealth.fillMethod = Image.FillMethod.Horizontal;
+        playerOneHealth.fillOrigin = (int) Image.OriginHorizontal.Left;
+        playerTwoHealth.type = Image.Type.Filled;
+        playerTwoHealth.fillMethod = Image.FillMethod.Horizontal;
+        playerTwoHealth.fillOrigin = (int)Image.OriginHorizontal.Right;
+
+        playerOneEmpty.type = Image.Type.Filled;
+        playerOneEmpty.fillMethod = Image.FillMethod.Horizontal;
+        playerOneEmpty.fillOrigin = (int)Image.OriginHorizontal.Left;
+        playerTwoEmpty.type = Image.Type.Filled;
+        playerTwoEmpty.fillMethod = Image.FillMethod.Horizontal;
+        playerTwoEmpty.fillOrigin = (int)Image.OriginHorizontal.Right;
+        playerOneEmpty.fillAmount = 1;
+        playerTwoEmpty.fillAmount = 1;
+
         playerOneSpecial.type = Image.Type.Filled;
         playerOneSpecial.fillMethod = Image.FillMethod.Vertical;
         playerTwoSpecial.type = Image.Type.Filled;
@@ -43,19 +62,95 @@ public class PlayerUI : MonoBehaviour
             playerTwoHealth.gameObject.SetActive(false);
             playerTwoBasic.gameObject.SetActive(false);
             playerTwoSpecial.gameObject.SetActive(false);
+            playerTwoEmpty.gameObject.SetActive(false);
         }
+
+        missionObjective.gameObject.SetActive(false);
+
+        setCharacters();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        updateHealth();
+        updateLives();
+        pauseObjective();
 
-	}
+        if (playerOne.GetComponentInParent<Player>().health <= 0 && gameHandler.GetComponent<GameHandler>().totalLives <= 0)
+        {
+            killCharacter(1);
+        }
 
+        if (twoPlayers && playerTwo.GetComponentInParent<Player>().health <= 0 && gameHandler.GetComponent<GameHandler>().totalLives <= 0)
+        {
+            killCharacter(2);
+        }
+    }
+
+    void setCharacters ()
+    {
+        playerOneName.text = GameHandler.selectedCharacterNamePlayerOne;
+
+        if (twoPlayers)
+        {
+            playerTwoName.text = GameHandler.selectedCharacterNamePlayerTwo;
+        }
+    }
+
+    void killCharacter (int playerToDelete)
+    {
+        if (playerToDelete == 1)
+        {
+            playerOneName.gameObject.SetActive(false);
+            playerOneHealth.gameObject.SetActive(false);
+            playerOneBasic.gameObject.SetActive(false);
+            playerOneSpecial.gameObject.SetActive(false);
+            playerOneEmpty.gameObject.SetActive(false);
+        }
+
+        else
+        {
+            playerTwoName.gameObject.SetActive(false);
+            playerTwoHealth.gameObject.SetActive(false);
+            playerTwoBasic.gameObject.SetActive(false);
+            playerTwoSpecial.gameObject.SetActive(false);
+            playerTwoEmpty.gameObject.SetActive(false);
+        }
+    }
+
+    void updateHealth ()
+    {
+        playerOneHealth.fillAmount = playerOne.GetComponentInParent<Player>().health / playerOne.GetComponentInParent<Player>().baseHealth;
+        if (twoPlayers)
+        {
+            playerTwoHealth.fillAmount = playerTwo.GetComponentInParent<Player>().health / playerTwo.GetComponentInParent<Player>().baseHealth;
+        }
+    }
+    void updateLives ()
+    {
+        lives.text = "Lives: " + gameHandler.GetComponent<GameHandler>().totalLives;
+    }
+
+    void pauseObjective ()
+    {
+        if (gameHandler.GetComponent<GameHandler>().stopGame && !missionObjective.gameObject.activeSelf)
+        {
+            missionObjective.gameObject.SetActive(true);
+            pauseOn = true;
+        }
+
+        if (pauseOn && !gameHandler.GetComponent<GameHandler>().stopGame)
+        {
+            pauseOn = false;
+            missionObjective.gameObject.SetActive(false);
+        }
+    }
     // This function will allow other scripts to update various text fields
     public void updateObjective (string inputText)
     {
-
+        missionObjective.text = inputText;
+        StartCoroutine(objectiveTimer());
     }
 
     public void startBasicCooldown(int playerNumber, float inputCoolDown)
@@ -101,5 +196,20 @@ public class PlayerUI : MonoBehaviour
 
             yield return null;
         } while (ourCooldown / cooldown != 1.0f);
+    }
+
+    IEnumerator objectiveTimer ()
+    {
+        float timePassed = 0;
+        do
+        {
+            if (!missionObjective.gameObject.activeSelf)
+            {
+                missionObjective.gameObject.SetActive(true);
+            }
+            timePassed += Time.deltaTime;
+            yield return null;
+        } while (timePassed < objectiveShowTime);
+        missionObjective.gameObject.SetActive(false);
     }
 }
